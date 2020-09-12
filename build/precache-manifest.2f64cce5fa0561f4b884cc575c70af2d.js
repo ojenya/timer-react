05571dfb05bebc3fced75e75e9c6ece4bab2e09b
Jenya <jeenjenya@gmail.com>
2020-09-13 01:08:28 +0300

    new build

---
+++ build/precache-manifest.2f64cce5fa0561f4b884cc575c70af2d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5836eba65a61fae27c8ffed34ca150e6",
+    "url": "/timer-react/index.html"
+  },
+  {
+    "revision": "c0171fa1a4525a68e240",
+    "url": "/timer-react/static/css/main.f4ad03fa.chunk.css"
+  },
+  {
+    "revision": "6d259e63b96464dd92a2",
+    "url": "/timer-react/static/js/2.dbac3847.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/timer-react/static/js/2.dbac3847.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0171fa1a4525a68e240",
+    "url": "/timer-react/static/js/main.bb4514ba.chunk.js"
+  },
+  {
+    "revision": "5a148b1845eae5ee1eaa",
+    "url": "/timer-react/static/js/runtime-main.06bad5cf.js"
+  },
+  {
+    "revision": "ee28f7f4058a63c5fb40354ee30b6251",
+    "url": "/timer-react/static/media/snow.ee28f7f4.jpeg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0b2b63e41b438d47a7490aa310a67312.js"
+  "/timer-react/precache-manifest.2f64cce5fa0561f4b884cc575c70af2d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/timer-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
